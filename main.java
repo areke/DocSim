80d6d1d899cbddc00ae9fd3fae30e4d87ff01637
areke <markpklein@gmail.com>
2014-03-11 23:00:24 +0000

    I forgot to push my other branch... Here it is. This is one with more customizable features. You can specify the number of words to be compared, as well as the directory to search.

--- main.java
+++ main.java
@@ -1,15 +1,76 @@
-public class Main {
-	public static void main(String[] args) {
-		int length = args.length;
-		if (length <= 0 || length > 2) {
-			System.out.println("Usage: java main.java <directory> [<words>].")
-		}
-		else if (length == 1) {
-			String dir = args[0];
-		}
-		else if {
-			int words = Integer.parseInt(args[1]);
-		}
-		
-	}
+import java.util.Scanner;
+import java.util.Arrays;
+public class main {
+  static utils util = new utils();
+  public static void exit() {
+    System.out.println("Usage: java main.java <directory> <words> [-explain -help].");
+    System.exit(0);
+  }
+ public static void main(String[] args) {
+  if (Arrays.asList(args).contains("-help")) {
+    exit(); 
+  }
+  int length = args.length;
+  if (length <= 0) {
+    util = new utils();
+  }
+  else if (length == 1) {
+    try {
+      int words = Integer.parseInt(args[0]);
+      util = new utils(words);
+    } catch (Exception e) {
+      String dir = args[0];
+      util = new utils(dir);
+    }
+    
+  }
+  else {
+    String dir = args[0];
+    try {
+      int words = Integer.parseInt(args[1]);
+      util = new utils(words, dir);
+    } catch (NumberFormatException e) {
+      if (!(args[1].equals("-explain") || args[1].equals("-help"))) {
+        exit();
+      }
+      else {
+        util = new utils(dir);
+      }
+      
+    }
+  }
+  if (Arrays.asList(args).contains("-explain")) {
+     System.out.println("The files being read are in the example directory");
+     System.out.println("There are 4 files, dog.txt, dog2.txt, cat.txt, and nobody.txt");
+     System.out.println("They have been compared by finding the cosine similarity between the documents");
+     System.out.println("The document is broken up into an array of words.");
+     System.out.println("Consider dog.txt and cat.txt:");
+     System.out.println("dog.txt contains the text 'the dog ran'");
+     System.out.println("cat.txt contains the text 'the cat ran'");
+     System.out.println("Arrays are created");
+     System.out.println("{'dog', 'ran', 'the'}");
+     System.out.println("{'cat', 'ran', 'the'}");
+     System.out.println("then they are merged into hashtables with the same keys");
+     System.out.println("with the values being the number of times each key appears");
+     System.out.println("in the corresponding string");
+     System.out.println("{'cat': 0, 'dog': 1, 'ran': 1, 'the': 1}");
+     System.out.println("{'cat': 1, 'dog': 0, 'ran': 1, 'the': 1}");
+     System.out.println("They are then related to the vectors");
+     System.out.println("{0, 1, 1, 1}, and");
+     System.out.println("{1, 0, 1, 1} respectively.");
+     System.out.println("The angle between these vectors is then found to determine the similarity");
+     System.out.println("between the documents.");
+     System.out.println("\n\n");
+     if (args.length == 1) {
+       exit();
+     }
+  }
+  
+
+   double[] results = util.everything();
+   for (int i = 0; i < results.length; i++) {
+     System.out.println(results[i]);
+   }
+  
+ }
 }
--- utils.java
+++ utils.java
@@ -6,170 +6,280 @@ import java.util.Hashtable;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+import java.util.Iterator;
 
-public class Utils {
+public class utils {
+ private static String dir = "docs";
+ private static int words = 1;
+ /**
+ * @return    an array containing all doc files in the user's current directory
+ */
+ public utils() {
+ }
+ public utils(int i) {
+   if (i<1) {
+     words = 1;
+   }
+   else if (i>10) {
+     words = 10;
+   }
+   else {
+     words = i;
+   }
+    
+ }
+ public utils(String a) {
+   if (Character.toString(a.charAt(0)) == "/") {
+     dir = a.substring(1, a.length());
+   }
+   else {
+     dir = a;
+   }
+    
+ }
+ public utils(int i, String a) {
+   if (Character.toString(a.charAt(0)) == "/") {
+     dir = a.substring(1, a.length());
+   }
+   else {
+     dir = a;
+   }
+   if (i<1) {
+     words = 1;
+   }
+   else if (i>10) {
+     words = 10;
+   }
+   else {
+     words = i;
+   }
+    
+ }
+ public static File[] getFiles() {
+     String[] extensions = {"doc", "txt", "docx", "rtf"};
+     String directory = System.getProperty("user.dir")+"/"+dir;
+     File directoryContents = new File(directory);
+     File[] filesList = directoryContents.listFiles();
+     File[] files = new File[filesList.length];
+     int num = 0;
+     for (int i=0; i < filesList.length; i++) {
+       String[] nameParts = filesList[i].getName().split("\\.");
+       if (Arrays.asList(extensions).contains(nameParts[1].toLowerCase())) {
+         files[num] = filesList[i];
+         num+=1;
+       }
+     }
+     File[] onlyFiles = new File[num];
+     for (int i=0; i < num; i++) {
+      onlyFiles[i] = files[i];
+     }
+     return onlyFiles;
+ }
 
-	/**
-	* @return    an array containing all doc files in the user's current directory
-	*/
-	public static File[] getFiles() {
-		String[] extensions = {"doc", "txt", "docx", "rtf"}
-	    String directory = System.getProperty("user.dir");
-	    File directoryContents = new File(directory);
-	    File[] filesList = directoryContents.listFiles();
-	    File[] files = new File[filesList.length];
-	    int num = 0;
-	    for (int i=0; i < filesList.length; i++) {
-	      String[] nameParts = filesList[i].getName().split("\\.");
-	      if (extensions.indexOf(nameParts[1].toLowerCase()) != -1) {
-	        files[num] = filesList[i];
-	        num+=1;
-	      }
-	    }
-	    File[] onlyFiles = new File[num];
-	    for (int i=0; i < num; i++) {
-	    	onlyFiles[i] = files[i];
-	    }
-	    return onlyFiles;
-	}
+ /**
+ * @param files    the array of files to be read
+ * @return         a two-dimensional array containing the different lines from each file.
+ */
+ public static String[][] readFiles(File[] files) {
+  //10000 line limit per file
+  String[][] contents = new String[files.length][10000];
+  for (int i = 0; i < files.length; i++) {
+   BufferedReader in = null;
+   try {
+    String line; 
+    in = new BufferedReader(new FileReader(files[i]));
+    int counter = 0;
+    while ((line = in.readLine()) != null) {
+     contents[i][counter] = line;
+    }
+   }
+   catch (IOException e) {
+    e.printStackTrace();
+   }
+   finally {
+    try {
+     if (in != null) {
+      in.close();
+     }
+    }
+    catch (IOException ex) {
+     ex.printStackTrace();
+    }
+   }
+  }
+  return contents;
+ }
 
-	/**
-	* @param files    the array of files to be read
-	* @return         a two-dimensional array containing the different lines from each file.
-	*/
-	public static String[][] readFiles(File[] files) {
-		//10000 line limit per file
-		String[][] contents = new String[files.length][10000];
-		for (int i = 0; i < files.length; i++) {
-			BufferedReader in = null;
-			try {
-				String line; 
-				in = new BufferedReader(new FileReader(files[i]));
-				int counter = 0;
-				while ((line = in.readLine()) != null) {
-					contents[i][counter] = line;
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-			finally {
-				try {
-					if (in != null) {
-						in.close();
-					}
-				}
-				catch (IOException ex) {
-					ex.printStackTrace();
-				}
-			}
-		}
-		return contents;
-	}
+ /**
+ * @param contents    the array of strings to be joined into one string
+ * @return            the string produced after joining the array of strings separated by a space
+ */
+ //join(String array,delimiter)
+public static String join(String[] contents)
+{
+        if (contents.length == 0) return "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<contents.length; i++) {
+            sb.append(contents[i]+" ");
+        }
+        return sb.toString();
+}
 
-	/**
-	* @param contents    the array of strings to be joined into one string
-	* @return            the string produced after joining the array of strings separated by a space
-	*/
-	public static String join(String[] contents) {
-		String content = StringUtils.join(contents, " ");
-		return content;
-	}
+ /**
+ * @param contents    the two-dimensional array whose elements should be joined into single strings
+ * @return            an array containing joined strings 
+ */
+ public static String[] join(String[][] contents) {
+  String[] newContents = new String[contents.length];
+  for (int i = 0; i < contents.length; i++) {
+   newContents[i] = join(contents[i]);
+  }
+  return newContents;
+ }
 
-	/**
-	* @param contents    the two-dimensional array whose elements should be joined into single strings
-	* @return            an array containing joined strings 
-	*/
-	public static String[] join(String[][] contents) {
-		String[] newContents = new String[contents.length];
-		for (int i = 0; i < contents.length; i++) {
-			newContents[i] = join(contents[i]);
-		}
-		return newContents;
-	}
+ /**
+ * @param str    the string whose non-alphanumeric characters should be stripped
+ * @return       a string containing only alphanumeric characters 
+ */
+ public static String removePunctuation(String str) {
+  return str.replaceAll("[^A-Za-z0-9 ]", "");
+ }
 
-	/**
-	* @param str    the string whose non-alphanumeric characters should be stripped
-	* @return       a string containing only alphanumeric characters 
-	*/
-	public static String removePunctuation(String str) {
-		return str.replaceAll("[^A-Za-z0-9]", "");
-	}
+ /**
+ * @param str    the string to be split up into an array
+ * @param n      the number of words to be included in each split
+ * @return       a new string split every nth word
+ */
+ public static String[] split(String str, int n) {
+   if (n < 1 || n > 10) {
+     n = 1;
+   }
+  String[] strings = str.split(" ");
+  String[] content = new String[strings.length/n];
+  int counter = 0;
+  for (int i = 0; i<content.length; i+=n) {
+    for (int a = 0; a < n; a++) {
+      if (!strings[i].equals("") && strings[i] != null) {
+        content[i] += strings[i+a]+" ";
+        content[i] = content[i].replaceAll("null","");
+        counter++;
+      }
+    }
 
-	/**
-	* @param str    the string to be split up into an array
-	* @param n      the number of words to be included in each split
-	* @return       a new string split every nth word
-	*/
-	public static String[] split(String str, int n) {
-		String dots = "";
-		for (int i = 0; i < n; i++) {
-			dots += ".";
-		}
-		String[] strings = java.util.Arrays.toString(s.split("(?<=\\G"+dots+")"));
-		return strings;
-	}
+    content[i] = content[i].substring(0, content[i].length()-1);
+  }
+  for (int i = 0; i < counter%n; i++) {
+    content[i] += "a ";
+  }
+  return content;
+ }
+ 
 
-	/**
-	* @param contents    the array of strings to be made into a hashtable
-	* @return            a hashtable with each distinct word in contents as a key and the number of times 
-	*                    that word appears as its value
-	*/
-	public static Hashtable<String, Integer> createHash(String[] contents) {
-		Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
-		for (int i = 0; i < contents.length; i++) {
-			String key = contents[i];
-			if (numbers.containsKey(key)) {
-				numbers.put(key, numbers.get(key) + 1);
-			}
-			else {
-				numbers.put(key, 1);
-			}
-		}
-		return numbers;
-	}
 
-	/**
-	* @param a    a hashtable to be merged
-	* @param b    a hashtable to be merged
-	* @return     the merged hashtable
-	*/
-	public static Hashtable<String, Int> merge(Hashtable<String, int> a, Hashtable<String, int> b) {
-		Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
-		Set<String> aKeys = a.keySet();
-		Set<String> bKeys = b.keySet();
-		for (String key : aKeys) {
-			int value = aKeys.get(key);
-			numbers.put(key, value);
-		}
-		for (String key : bKeys) {
-			int value = bKeys.get(key);
-			if (!numbers.containsKey(key)) {
-				numbers.put(key, 0);
-			}
-		}
-		return numbers;
-	}
+ /**
+ * @param contents    the array of strings to be made into a hashtable
+ * @return            a hashtable with each distinct word in contents as a key and the number of times 
+ *                    that word appears as its value
+ */
+ public static Hashtable<String, Integer> createHash(String[] contents) {
+  Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
+   for (String content : contents) {
+     System.out.println(content);
+   }
+  for (int i = 0; i < contents.length; i++) {
+   String key = contents[i];
+   if (!key.equals("") && key != null) {
+     if (numbers.containsKey(key)) {
+       numbers.put(key, numbers.get(key) + 1);
+     }
+     else {
+       numbers.put(key, 1);
+     }
+   }
+  }
+  return numbers;
+ }
 
-	/**
-	* @param a    a hastable of strings with their values
-	* @return     an array of integers
-	*/
-	public static int[] toArray(Hashtable<String, int> a) {
-		int[] numbers = new int[a.size()];
-		Set<String> keys = a.keySet();
-		int counter = 0;
-		for (String key : keys) {
-			int value = keys.get(key);
-			numbers[counter] = value;
-			counter++;
-		}
-		return numbers;
+ /**
+ * @param a    a hashtable to be merged
+ * @param b    a hashtable to be merged
+ * @return     the merged hashtable
+ */
 
-	}
+ public static Hashtable<String, Integer> merge(Hashtable<String, Integer> a, Hashtable<String, Integer> b) {
+  Hashtable<String, Integer> numbers = new Hashtable<String, Integer>();
+  Set<String> aKeys = a.keySet();
+  Set<String> bKeys = b.keySet();
+  for (String key : aKeys) {
+   int value = a.get(key);
+   numbers.put(key, value);
+  }
+  for (String key : bKeys) {
+   int value = b.get(key);
+   if (!numbers.containsKey(key)) {
+    numbers.put(key, 0);
+   }
+  }
+  return numbers;
+ }
 
-	// Move to a different file later 
-	public static String everything(String[][] contents) {
-	}
+
+ /**
+ * @param a    a hastable of strings with their values
+ * @return     an array of integers
+ */
+   
+ public static int[] toArray(Hashtable<String, Integer> a) {
+  int[] numbers = new int[a.size()];
+  List<String> tmp = Collections.list(a.keys());
+    Collections.sort(tmp);
+    Iterator<String> it = tmp.iterator();
+    int counter = 0;
+    while(it.hasNext()){
+        String element = it.next();
+         int value = a.get(element);
+         numbers[counter] = value;
+         counter++;
+    }
+  return numbers;
+
+ }
+
+ /**
+ * @return the results of comparing every document in the folder.
+ */
+ public static double[] everything() {
+   File[] files = getFiles();
+   String[][] readfiles = readFiles(files);
+   String[] content = new String[readfiles.length];
+   String[][] strings = new String[readfiles.length][10000];
+   for (int i = 0; i < readfiles.length; i++) {
+       content[i] = join(readfiles[i]);
+       content[i] = removePunctuation(content[i]);
+       strings[i] = split(content[i], words);
+   }
+   double[] results = new double[(content.length*(content.length-1)/2)];
+   int counter = 0;
+   String[][] stuffs = new String[results.length][2];
+   for (int i = 0; i < content.length; i++) {
+     for (int k = i+1; k < content.length; k++) {
+       Hashtable<String, Integer> a = createHash(strings[i]);
+       Hashtable<String, Integer> b = createHash(strings[k]);
+       Hashtable<String, Integer> mergeda = merge(a, b);
+       Hashtable<String, Integer> mergedb = merge(b, a);
+       int[] arra = toArray(mergeda);
+       int[] arrb = toArray(mergedb);
+       DocMath doc = new DocMath();
+       double cos = doc.cosSimilarity(arra, arrb);
+       results[counter] = cos;
+       counter++;
+
+     }
+   }
+   return results;
+ }
 
 }
